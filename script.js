f7748949ed6eabbbcc4e049c06ca9ff5e4ebf7b9
Mithleshkumar5360 <117704988+Mithleshkumar5360@users.noreply.github.com>
2023-04-19 15:18:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+
+// Create the student object with a name property
+const student = {
+  name: 'John Doe'
+};
+
+// Add a getKeys method to the Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Usage
+console.log(student.getKeys()); // Output: ['name']
